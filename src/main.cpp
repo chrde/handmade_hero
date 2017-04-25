1fe29f1d094eead995e1195a93bff06faa50a3e8
Christian de la Hoz <apigolas@gmail.com>
2017-04-25 21:45:51 +0200

    day 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,64 @@
 #include <windows.h>
 
-LRESULT CALLBACK MainWindowCallback(HWND window, UINT message, WPARAM wParam,
-                                    LPARAM lParam) {
+#define internal static
+#define local_persist static
+#define global_variable static
+
+// TODO change this
+global_variable bool running;
+global_variable BITMAPINFO bitmapInfo;
+global_variable void *bitmapMemory;
+global_variable HBITMAP bitmapHandle;
+global_variable HDC bitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int width, int height) {
+  // TODO improve
+
+  if (bitmapHandle) {
+    DeleteObject(bitmapHandle);
+  }
+  if (!bitmapDeviceContext) {
+    // TODO should we recreate it?
+    bitmapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  BITMAPINFOHEADER *header = &bitmapInfo.bmiHeader;
+  header->biSize = sizeof(*header);
+  header->biWidth = width;
+  header->biHeight = height;
+  header->biPlanes = 1;
+  header->biBitCount = 32;
+  header->biCompression = BI_RGB;
+
+  bitmapHandle = CreateDIBSection(bitmapDeviceContext, &bitmapInfo,
+                                  DIB_RGB_COLORS, &bitmapMemory, 0, 0);
+}
+
+internal void Win32UpdateWindow(HDC deviceContext, int x, int y, int width,
+                                int height) {
+  StretchDIBits(deviceContext, x, y, width, height, x, y, width, height,
+                bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(HWND window, UINT message,
+                                         WPARAM wParam, LPARAM lParam) {
   LRESULT result = 0;
   switch (message) {
     case WM_SIZE: {
-      OutputDebugString("WM_SIZE");
+      RECT c;
+      GetClientRect(window, &c);
+      int width = c.right - c.left;
+      int height = c.bottom - c.top;
+      Win32ResizeDIBSection(width, height);
       break;
     }
     case WM_DESTROY: {
-      OutputDebugString("WM_DESTROY");
+      // TODO try to reopen window
+      running = false;
       break;
     }
     case WM_CLOSE: {
-      OutputDebugString("WM_CLOSE");
+      running = false;
       break;
     }
     case WM_PAINT: {
@@ -24,12 +69,12 @@ LRESULT CALLBACK MainWindowCallback(HWND window, UINT message, WPARAM wParam,
       int y = rect.top;
       int height = rect.bottom - rect.top;
       int width = rect.right - rect.left;
-      PatBlt(deviceContext, x, y, width, height, WHITENESS);
+      Win32UpdateWindow(deviceContext, x, y, width, height);
       EndPaint(window, &paint);
       break;
     }
     case WM_ACTIVATEAPP: {
-      OutputDebugString("WM_ACTIVATEAPP");
+      OutputDebugString("WM_ACTIVATEAPP\n");
       break;
     }
     default: {
@@ -45,7 +90,7 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance,
   // MessageBox(0, "Hello", "Title", MB_OK | MB_ICONINFORMATION);
   WNDCLASS WindowClass = {};
   WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.lpfnWndProc = Win32MainWindowCallback;
   WindowClass.hInstance = instance;
   WindowClass.lpszClassName = "MyWindowClass";
   if (RegisterClass(&WindowClass)) {
@@ -54,8 +99,9 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance,
         WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
         CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, instance, 0);
     if (windowHandle) {
+      running = true;
       MSG message;
-      while (1) {
+      while (running) {
         BOOL messageResult = GetMessage(&message, 0, 0, 0);
         if (messageResult > 0) {
           TranslateMessage(&message);
