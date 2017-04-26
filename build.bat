54835739d58a52472dc290e00d7020133c8373b8
Christian de la Hoz <apigolas@gmail.com>
2017-04-26 15:03:10 +0200

    build script and vscode

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
-
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+set compilerFlags=/Zi /FC
+set deps=user32.lib Gdi32.lib
+set main=..\src\main.cpp
 mkdir .\build
 pushd .\build
-cl -FC -Zi ..\src\main.cpp user32.lib Gdi32.lib
+cl %compilerFlags% %main% %deps%
 popd
